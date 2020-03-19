70f9e49a7d9d9b6f6033a6b3f5fca1118f0c4297
patrick-replogle <patrickr1138@gmail.com>
2020-03-19 11:12:27 -0700

    following along

--- client/src/client.js
+++ client/src/client.js
@@ -1,9 +1,27 @@
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import gql from 'graphql-tag'
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
 
+const link = new HttpLink({ uri: "https://rickandmortyapi.com/graphql/" });
+const cache = new InMemoryCache();
 
-const client = new ApolloClient()
+const client = new ApolloClient({
+  link,
+  cache
+});
 
-export default client
+const query = gql`
+  {
+    characters {
+      results {
+        id
+        name
+      }
+    }
+  }
+`;
+
+client.query({ query }).then(result => console.log(result));
+
+export default client;
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,15 @@
-import { Switch, Route } from 'react-router-dom'
-import React, {Fragment} from 'react'
-import Header from './Header'
-import Pets from '../pages/Pets'
+import { Switch, Route } from "react-router-dom";
+import React, { Fragment } from "react";
+import Header from "./Header";
+import Pets from "../pages/Pets";
 
 const App = () => (
   <Fragment>
     <Header />
     <div>
-      <Switch>
-        <Route exact path="/" component={Pets} />
-      </Switch>
+      <Switch>{/* <Route exact path="/" component={Pets} /> */}</Switch>
     </div>
   </Fragment>
-)
+);
 
-export default App
+export default App;
